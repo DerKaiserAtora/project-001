bfe9843b8cac02069c94a093466c0fe86c10324a
DerKaiserAtora <suzakuflamepheonix@gmail.com>
2019-05-14 02:11:20 +0700

    git bash B002 commit again

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -37,7 +37,7 @@ public class HomeController extends Controller {
 	/*this commit is using intellij*/
 	/*this commit is using cmd*/
 	/*sadfasdfasdfasdfasdfasdfasdfasdf*/
-	/*this commit is using intellij only to make conflict again*/
+	/*this commit is using intellij only to make conflict again*//*afadfasfaskdjfhalsdkhfalhdfajkhdfkjahsdkfhakdshfklasdf*/
     /*line 1*/
     /*line 2*/
     /*line 3*//*adsfasdfasdfasdfasdfasdfasdfasdfasadasfasdfasdfasjkdfa*/
